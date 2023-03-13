82b9374c993ef3f7fd82e92d645bd08f806eac8e
Shreenath Ware <124200731+shreenath9208@users.noreply.github.com>
2023-03-13 15:05:02 +0530

    Add files via upload
    This is the  Interncrowd Landing  web  page Code

---
+++ dom.js
@@ -0,0 +1,7 @@
+function mouse(){
+    document.getElementById("card").style.backgroundColor="red";
+    document.getElementById("card1").style.backgroundColor="blue";
+    document.getElementById("card2").style.backgroundColor="grey";
+    document.getElementById("card3").style.backgroundColor="yellow";
+
+};
